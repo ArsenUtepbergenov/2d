bcf573f8fddd1306981daa96ee6ae86797d84e73
ArsenUtepbergenov <arsen.utepbergenov@yandex.ru>
2022-12-03 21:34:12 +0100

    1

--- src/entities/physics/Engine.ts
+++ src/entities/physics/Engine.ts
@@ -1,14 +1,8 @@
 import PhysicsTrait from './traits/PhysicsTrait'
 import { v4 as uuidv4 } from 'uuid'
-import Entity from './Entity'
-import Vector from '../math/Vector'
 
 export default class Engine {
   private physicsTraits: Map<string, PhysicsTrait> = new Map()
-
-  public updateVelocity(entity: Entity, vector: Vector): void {
-    entity.velocity = vector
-  }
 
   public getPhysicsTrait(): PhysicsTrait {
     const pt = new PhysicsTrait()
